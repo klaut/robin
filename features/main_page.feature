8e46d5603be0777b9cef5673325d8db80a00a246
Tanja Pislar <tanja.pislar@gmail.com>
2013-02-16 00:01:52 +0000

    cucumber main page steps should involve Manager

--- features/main_page.feature
+++ features/main_page.feature
@@ -3,3 +3,6 @@ Feature: Users can connect to Redis
   Scenario: User can connect to Redis
     When I insert a valid Redis url
     Then I should be connected to Redis
+
+  Scenario: Bad redis connection
+    When I insert a bad url "bad" I see an error message
--- features/step_definitions/main_page_steps.rb
+++ features/step_definitions/main_page_steps.rb
@@ -1,9 +1,16 @@
-require_relative "../../lib/services/redis_service"
+require_relative "../../lib/services/manager"
 
 When /^I insert a valid Redis url$/ do
-  @redis = RedisService.for_url(REDIS_TEST_URL)
+  @redis_manager = Manager.connect(REDIS_TEST_URL)
 end
 
 Then /^I should be connected to Redis$/ do 
-  @redis.should be_kind_of RedisService
+  @redis_manager.service.should be_kind_of RedisService
 end
+
+When /^I insert a bad url "(.*?)" I see an error message$/ do |url|
+  expect {
+    @redis_manager = Manager.connect url
+  }.to raise_error(RedisService::ResourceError)
+end
+
