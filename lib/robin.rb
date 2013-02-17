ab356e37c8641f1d8725b03a2eb4ac95b8703908
Tanja Pislar <tanja.pislar@gmail.com>
2013-02-17 10:54:07 +0000

    first draft of the basic html view

--- lib/robin.rb
+++ lib/robin.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
+require_relative 'services/manager'
 
 class Robin < Sinatra::Base
   get '/' do
-    'Hello Robin!'
+    erb :index
+  end
+
+  post '/connect' do
+    begin
+      @manager = Manager.connect params[:redis_address]
+      "hey, #{@manager.list_keys}"
+    rescue
+      "ooops"
+    end
   end
 
   # start the server if ruby file executed directly
